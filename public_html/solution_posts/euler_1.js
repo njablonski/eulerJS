97538d50c993715d0efb2dc45bdd8491657ae790
Nicholas Jablonski <njablonski@gmail.com>
2017-05-05 05:42:08 +0000

    Finalize styling, finish solution page for problem 1

--- public_html/solution_posts/euler_1.js
+++ public_html/solution_posts/euler_1.js
@@ -1,10 +1,13 @@
 function main() {
 
+  // initialize aggregator and incrementor variables
   var sum = 0;
   var count = 0;
 
+  // loop increments count through range 0-999
   while (count < 1000) {
 
+    // add count to sum if it's divisible by 3 or 5
     if ((count % 3 == 0) || (count % 5 == 0)) {
       sum += count;
     }
@@ -12,6 +15,7 @@ function main() {
     count += 1;
   }
 
+  // write the final value of sum to the output area
   document.getElementById("outputBox").innerHTML = sum;
 
 }
