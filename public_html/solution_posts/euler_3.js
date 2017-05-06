8a0a593e866d35efa64989209e02b745454001c6
Nicholas Jablonski <njablonski@gmail.com>
2017-05-06 03:55:05 +0000

    Complete page for problem 3

--- public_html/solution_posts/euler_3.js
+++ public_html/solution_posts/euler_3.js
@@ -1,7 +1,9 @@
 function getLargestPrime(num) {
 
+  // we check for factors up to the square root of the input
   for(i = 2; i <= Math.sqrt(num); i++) {
 
+    // if we find a factor, we divide it out of the input and recurse
     if(num % i == 0) {
 
       num /= i;
@@ -11,12 +13,16 @@ function getLargestPrime(num) {
 
   }
 
+  // if we make it here, we've divided out all smaller prime factors and num is our answer
   return num;
 
 }
 
-function computeResult() { 
+function main() { 
 
+  // write the result of getLargestPrime() on the given number to the output area
   document.getElementById("outputBox").innerHTML = getLargestPrime(600851475143);
 
 }
+
+main();
