c4445a7b2b0dcfef1539e4dd63f90ab2cf6e066a
Nicholas Jablonski <njablonski@gmail.com>
2017-05-06 03:21:51 +0000

    Complete solution page for problem 2

--- public_html/solution_posts/euler_2.js
+++ public_html/solution_posts/euler_2.js
@@ -1,26 +1,33 @@
-function computeResult() {
+function main() {
 
+  // three variables are needed to generate the sequence
+  var currentNum = 2;
   var lastNum = 1;
-  var lastLastNum = 0;
-  var currentNum = 1;
   var buffer = 0;
+
+  // plus one to hold our running sum
   var sum = 0;
 
-
+  // loop generates sequence until it exceeds 4 million
   while (currentNum < 4000000) {
 
-    currentNum = lastNum + lastLastNum;
-    lastLastNum = lastNum;
-    lastNum = currentNum;
-
+    // if the current number is even, add it to our sum
     if (currentNum % 2 == 0) {
 
       sum += currentNum;
 
     }
 
+    // generate the next number
+    buffer = currentNum;
+    currentNum += lastNum;
+    lastNum = buffer;
+
   }
 
+  // write sum to output area
   document.getElementById("outputBox").innerHTML = sum;
 
 }
+
+main();
